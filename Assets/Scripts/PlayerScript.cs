b74a90ea958a6298c8709c03a0d49381a1904df2
jwise1596 <jwise1596@knights.ucf.edu>
2020-10-25 20:00:19 -0400

    Challenge commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,41 +4,79 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour
-{
+{   
     private Rigidbody2D rd2d;
     public float speed;
     public Text score;
+    public Text win;
+    public Text lives;
     private int scoreValue = 0;
+    private int livesValue = 3;
+    public AudioSource musicSource;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
+        win.text = "";
+        lives.text = livesValue.ToString();
+        musicSource.clip = musicClipOne;
+        musicSource.Play();
+        musicSource.loop = true;
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
     
+    // Update is called once per frame
+    void FixedUpdate()     
     {
-        if (Input.GetKey("escape"))
-{
-Application.Quit();
-}
         float hozMovement = Input.GetAxis("Horizontal");
         float vertMovement = Input.GetAxis("Vertical");
         rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+        
+        if (Input.GetKey("escape"))
+            {
+                Application.Quit();
+            }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+private void OnCollisionEnter2D(Collision2D collision)
     {
        if (collision.collider.tag == "Coin")
-        {
-            scoreValue += 1;
-            score.text = scoreValue.ToString();
-            Destroy(collision.collider.gameObject);
+        {    
+                scoreValue += 1;
+                score.text = scoreValue.ToString();
+                Destroy(collision.collider.gameObject);
+            
+            if (scoreValue == 4)
+            {
+                transform.position= new Vector2(55.0f, 0);
+                livesValue =3;
+                lives.text = livesValue.ToString();
+            }
+            if (scoreValue == 8)
+            {
+                win.text = "You Win! Game created by Jessica Wise.";
+                {
+                    musicSource.clip = musicClipTwo;
+                    musicSource.Play();
+                }
+            }
         }
+        if (collision.collider.tag == "Enemy")
+        {   
+                livesValue -= 1;
+                lives.text = livesValue.ToString();
+                Destroy(collision.collider.gameObject);
 
+            if (livesValue == 0)
+            {
+                win.text = "You Lose! Game created by Jessica Wise";
+                Destroy(this);
+            }
+        }
     }
     private void OnCollisionStay2D(Collision2D collision)
     {
